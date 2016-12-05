cb003f5642b61f4f15453047824b02e320549eca
Parker Birchard <Parker_Birchard@bullis.org>
2016-12-05 17:16:35 -0500

    Made TeleOp basics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,19 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by pedropetcov on 12/5/16.
  */
-
-public class TeleOp extends LinearOpMode
-{
-
-    @Override
-    public void runOpMode() throws InterruptedException
+@TeleOp (name = "TeleOp Stuff", group = "TeleOp Stuff")
+public class TeleOp extends LinearOpMode {
     {
-    }
+        private DcMotor treadLeft;
+        private DcMotor treadRight;
 
+        @Override
+        public void runOpMode ()throws InterruptedException {
+        treadLeft = hardwareMap.dcMotor.get("treadLeft");
+        treadRight = hardwareMap.dcMotor.get("treadRight");
+
+        treadLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+         treadLeft.setPower(-gamepad1.left_stick_y);
+         treadRight.setPower(-gamepad1.right_stick_y);
+
+            idle();
+        }
+    }
+    }
 
 
 
