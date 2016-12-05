bf397ddc3fb88809c9a3b9a95ed8cbbaaeca8afb
Pedro Petcov <pedropetcov@Pedros-MacBook-Pro.local>
2016-12-05 16:41:56 -0500

    No downgrading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,8 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 /**
  * Created by pedropetcov on 12/5/16.
  */
 
-public class TeleOp {
+public class TeleOp extends LinearOpMode
+{
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+    }
 }
