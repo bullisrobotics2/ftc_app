a4d62b0cff284226394c582c5468152d09d444d9
Parker Birchard <Parker_Birchard@bullis.org>
2016-12-09 09:16:42 -0500

    Made the Autonomous phase, and commented on the code so other people can understand what is going on inside of the code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
@@ -14,16 +14,16 @@ package org.firstinspires.ftc.teamcode;
 @TeleOp (name = "TeleOp Stuff", group = "TeleOp Stuff")
 public class TeleOp2 extends LinearOpMode {
 
-    DcMotor treadLeft;
-    DcMotor treadRight;
-    DcMotor Pinball;
+    DcMotor treadLeft; //creates a motor for the left tread
+    DcMotor treadRight; //creates a motor for the right tread
+    DcMotor Pinball; // creates a motor for the pinball
 
     public void runOpMode() throws InterruptedException {
         treadLeft = hardwareMap.dcMotor.get("treadLeft");
         treadRight = hardwareMap.dcMotor.get("treadRight");
         Pinball = hardwareMap.dcMotor.get("Pinball");
 
-        treadLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        treadLeft.setDirection(DcMotorSimple.Direction.REVERSE); //sets the left tread to go reverse so we go forwards
 
         waitForStart();
 
@@ -35,19 +35,19 @@ public class TeleOp2 extends LinearOpMode {
             treadLeft.setPower(-gamepad2.left_stick_y);
             treadRight.setPower(-gamepad2.right_stick_y);
             Pinball.setPower(-gamepad2.right_trigger);
-
+            //sets the controls for the motors so they can control the robot
             idle();
         }
     }
 
-    public void particleLauncher() {
+    public void particleLauncher() { //creates the method for the particle launcher
         try {
-            if (-gamepad1.right_trigger == 1 || -gamepad2.right_trigger == 1) {
-                Pinball.setPower(50);
-                wait(2000);
+            if (-gamepad1.right_trigger == 1 || -gamepad2.right_trigger == 1) { //when these buttons are pressed do below
+                Pinball.setPower(50); //sets the motor power
+                wait(20000); //makes the robot wait for the time
             }
         }
-        catch (InterruptedException ex) {
+        catch (InterruptedException ex) { //fixes any interrupted exception errors
         }
     }
 }
